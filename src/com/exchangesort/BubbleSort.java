75bdcfaf774d716001af04b03727face2b06e9bd
zgzjj <252070739@qq.com>
2017-12-12 22:08:13 +0800

    冒泡排序

---
+++ src/com/exchangesort/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.exchangesort;
+
+//冒泡排序
+public class BubbleSort {
+    public void sort(int[] a) {
+        System.out.println("排序之前：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println();
+        System.out.println("=================================");
+        int temp;
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
+                if (a[j] > a[j + 1]) {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("排序之后：");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {66, 32, 78, 94, 81, 23, 15, 8, 10, 55};
+        BubbleSort bubbleSort = new BubbleSort();
+        bubbleSort.sort(a);
+    }
+}
